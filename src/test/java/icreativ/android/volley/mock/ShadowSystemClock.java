fc9617227e50b08aef98645937c090f217d7ab62
Shubham Kokadwar <sk23628@gmail.com>
2016-04-04 13:12:19 +0530

    commit1

---
+++ src/test/java/icreativ/android/volley/mock/ShadowSystemClock.java
@@ -0,0 +1,11 @@
+package icreativ.android.volley.mock;
+
+import android.os.SystemClock;
+import org.robolectric.annotation.Implements;
+
+@Implements(value = SystemClock.class, callThroughByDefault = true)
+public class ShadowSystemClock {
+    public static long elapsedRealtime() {
+        return 0;
+    }
+}
